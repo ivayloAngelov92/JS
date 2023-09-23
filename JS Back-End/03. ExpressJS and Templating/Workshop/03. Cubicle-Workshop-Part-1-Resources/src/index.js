3f45e3ff9676f2adb681a53f4f0b9b0ed84055b1
ivayloAngelov92 <i.r.angelov92@gmail.com>
2023-09-23 17:56:07 +0100

    ee
    update

--- JS Back-End/03. ExpressJS and Templating/Workshop/03. Cubicle-Workshop-Part-1-Resources/src/index.js
+++ JS Back-End/03. ExpressJS and Templating/Workshop/03. Cubicle-Workshop-Part-1-Resources/src/index.js
@@ -1,8 +1,17 @@
 const express= require('express');
+const handlebars= require('express-handlebars');
+
 const app= express();
 const PORT= 3000
+
+//handlebars config
+app.engine('hbs', handlebars.engine({extname:'hbs'}));
+app.set('view engine',	'hbs')
+app.set('views', 'src/views')
+
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.render('index')
 })
 
 app.listen(PORT, (req, res) => {console.log(`Server is Running on port: ${PORT}`);});
